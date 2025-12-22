cf8da15fbdd14a70d2a287ec42281f3294d10233
sumit619sharma <imsumitsharma619@gmail.com>
2025-12-22 16:19:22 +0530

    update drive location on user UI

--- backend/socket.js
+++ backend/socket.js
@@ -38,6 +38,24 @@ export function initializeSocket(server) {
           lng: location.lng,
         },
       });
+
+{/** 
+      // send updated location to user if there's an active ride
+      const captain = await captainModel.findById(userId).populate('currentRide');
+      if (captain.currentRide) { 
+        const ride = captain.currentRide;
+        const user = await userModel.findById(ride.userId);
+        if (user && user.socketId) {
+          io.to(user.socketId).emit("captain-location-updated", {
+            captainId: userId,
+            location,
+          });
+        } else {
+          console.log("User not found or has no socketId");
+        }
+      }
+*/}
+
     });
 
     socket.on("disconnect", () => {
